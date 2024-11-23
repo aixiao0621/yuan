7a3903a4d9da8a0afa537e8e85f8a31e4a1cd682
aixiao0621 <52861243+aixiao0621@users.noreply.github.com>
2024-11-24 01:38:46 +0800

    feat: 主页

--- app/src/main/java/az/winter/yuan/MainActivity.kt
+++ app/src/main/java/az/winter/yuan/MainActivity.kt
@@ -9,14 +9,32 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.dp
 import az.winter.yuan.ui.theme.YuanTheme
+import coil.ImageLoader
+import coil.compose.rememberImagePainter
+import coil.decode.ImageDecoderDecoder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,27 +42,116 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YuanTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    content = { paddingValues ->
-                        Column {
-                            Text(
-                            text = "Hello World",
-                            modifier = Modifier.padding(paddingValues)
-                            )
-                            Button(onClick = { setLiveWallpaper(this@MainActivity) }) {
-                                Text(text = "Button")
-                            }
-                        }
-                    }
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    RenderCardsWithSwipe(this@MainActivity)
+                }
             }
         }
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RenderCardsWithSwipe(context: Context) {
+    // 定义 PagerState 来管理当前页
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { 2 })
+
+    // 创建滑动分页
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 32.dp),
+        userScrollEnabled = true,
+        pageSpacing = 16.dp
+    ) { page ->
+        CardContent(page = page, context = context)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardContent(page: Int, context: Context) {
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            add(ImageDecoderDecoder.Factory())
+        }
+        .build()
+    when (page) {
+        0 -> {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.65f),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+                onClick = {
+                    setLiveWallpaper(context)
+                }
+            ) {
+                // 卡片内容
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = R.raw.mian_a,
+                            imageLoader = imageLoader,
+                            builder = {
+                                crossfade(true)
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize(1.0f)
+                            .scale(1.4f)
+                    )
+                }
+            }
+        }
+
+        1 -> {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.65f),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+                onClick = { Toast.makeText(context, "Card 2 Clicked", Toast.LENGTH_SHORT).show() }
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = R.raw.mian_a,
+                            imageLoader = imageLoader,
+                            builder = {
+                                crossfade(true)
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize(1.0f)
+                            .scale(1.4f)
+                    )
+                }
+            }
+        }
+    }
+}
+
 fun setLiveWallpaper(context: Context) {
     val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).apply {
-        putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+        putExtra(
+            WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
             ComponentName(context, MyWallpaperService::class.java)
         )
     }
