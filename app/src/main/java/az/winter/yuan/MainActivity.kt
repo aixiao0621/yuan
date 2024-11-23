c0dd52c2901e40c8447cf762cda3900476caaeec
aixiao0621 <52861243+aixiao0621@users.noreply.github.com>
2024-11-23 22:58:37 +0800

    chore: by jetpack compose

--- app/src/main/java/az/winter/yuan/MainActivity.kt
+++ app/src/main/java/az/winter/yuan/MainActivity.kt
@@ -1,16 +1,21 @@
 package az.winter.yuan
 
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import az.winter.yuan.ui.theme.YuanTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YuanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    content = { paddingValues ->
+                        Column {
+                            Text(
+                            text = "Hello World",
+                            modifier = Modifier.padding(paddingValues)
+                            )
+                            Button(onClick = { setLiveWallpaper(this@MainActivity) }) {
+                                Text(text = "Button")
+                            }
+                        }
+                    }
+                )
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YuanTheme {
-        Greeting("Android")
+fun setLiveWallpaper(context: Context) {
+    val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).apply {
+        putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+            ComponentName(context, MyWallpaperService::class.java)
+        )
+    }
+    try {
+        context.startActivity(intent)
+    } catch (e: Exception) {
+        Toast.makeText(context, "Failed to set live wallpaper.", Toast.LENGTH_SHORT).show()
+        e.printStackTrace()
     }
 }
